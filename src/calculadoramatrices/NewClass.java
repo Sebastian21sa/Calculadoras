01ec1019a77f94c6522bbfd32395eee689444fd1
Sebastian Sanchez <Sebastian Sanchez@DESKTOP-GCBTC4I>
2022-09-23 19:49:27 -0500

    1.0

---
+++ src/calculadoramatrices/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadoramatrices;
+
+/**
+ *
+ * @author Sebastian Sanchez
+ */
+public class NewClass implements IInterface{
+    public  void sumaMatrices() {
+         
+                 
+    }
+}
